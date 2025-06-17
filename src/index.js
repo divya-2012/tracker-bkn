b448fe4619e324bf484b579047cde31c022e6976
Logesh T V <logesh.tv@rigvedit.com>
2025-06-17 11:29:04 +0530

    asd

--- src/index.js
+++ src/index.js
@@ -14,7 +14,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware allow all CORS requests
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Welcome route
